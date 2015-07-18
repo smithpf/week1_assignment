b54a6fe8adafef9fb4d59fe267c53a4cd2692bc7
Brian Hogan / New Auburn Personal Computer Services <bphogan@gmail.com>
2015-07-18 13:38:02 -0500

    Feedback on guess_the_number assignment
    Good work. See comments.

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,12 +1,26 @@
-
-diff_choice = 0
 number_of_guesses = 0
+
+# I'm not sure what this variable means. You should not abbreviate variables. 
+# We aren't working in memory-constrained environments anymore. Characters
+# are cheap - time to understand code isn't.
+diff_choice = 0
+
 number = 0
 
 puts "Let's play the guess the number game"
 print "Please pick a difficulty level(1, 2, 3)"
 
 diff_choice = gets.chomp.to_i
+
+## This function should be defined more specifically.
+## What does this function do? It gets_computer_number
+## and it should take in the difficulty from the main
+## program. It should not read a global variable.
+#
+# def level(difficulty)
+#  case difficulty
+#   ...
+# end
 
 def level
   
@@ -36,6 +50,7 @@ loop do
   else
     puts "You guessed it!"
     puts "It took you #{number_of_guesses} guesses"
+    ## I'd break this out into a function that accepts the number of guesses and returns a string.
     case number_of_guesses
     when 1
       puts "You're a mind reader!"
@@ -48,4 +63,5 @@ loop do
     end
     exit
   end
+  ## where's the code to play again?
 end
